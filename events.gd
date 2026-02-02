8e05545cf4d11c0d1bc94141f1326757607b9869
Asreonn <asrinylmz2@gmail.com>
2026-02-02 04:27:55 +0300

    Refactor: Decouple asteroid effects

--- events.gd
+++ events.gd
@@ -5,3 +5,5 @@ signal player_exited_base_station(base: Node2D, player: Node2D)
 
 signal laser_damage_requested(target: Node2D, amount: float)
 signal laser_impact_pulse_requested(target: Node2D)
+
+signal camera_shake_requested(intensity: float)
