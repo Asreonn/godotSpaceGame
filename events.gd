af2b8631dce17980e51ac80913ce92c55c44295f
Asreonn <asrinylmz2@gmail.com>
2026-02-02 04:18:37 +0300

    Route laser damage through Events

--- events.gd
+++ events.gd
@@ -2,3 +2,6 @@ extends Node
 
 signal player_entered_base_station(base: Node2D, player: Node2D)
 signal player_exited_base_station(base: Node2D, player: Node2D)
+
+signal laser_damage_requested(target: Node2D, amount: float)
+signal laser_impact_pulse_requested(target: Node2D)
